9e99dbe365facffce4c0814d81be2e5e024b000a
Daiquiry <nil.or91@gmail.com>
2024-06-08 15:08:44 +0200

    init

---
+++ git_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Add all files to the staging area
+git add *
+
+# Commit the changes with the message "init"
+git commit -m "init"
+
+# Push the changes to the remote repository
+git push
+
